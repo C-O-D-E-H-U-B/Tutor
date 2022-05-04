9d274d44a675c0e21e7af0c4d9da1f16a17ea243
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2022-05-04 23:20:18 +0530

    Initial Commit

---
+++ src/test/java/com/codehub/tutor/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codehub.tutor.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
