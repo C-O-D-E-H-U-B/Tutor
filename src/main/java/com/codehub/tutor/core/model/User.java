d72f18b2ade8c22a183b3ed531dd94d558680cfa
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2022-06-05 20:39:19 +0530

    [users] - Add user model

---
+++ src/main/java/com/codehub/tutor/core/model/User.java
@@ -0,0 +1,120 @@
+package com.codehub.tutor.core.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class User {
+    private String displayName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "userId")
+    private Long userId;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "permissions")
+    private String permissions;
+    @Column(name = "settings")
+    private String settings;
+    @Column(name = "type")
+    private String type;
+    @Column(name = "dob")
+    private String dob;
+    @Column(name = "status")
+    private int status;
+    @Column(name = "profilePic")
+    private String profilePic;
+    @Column(name = "studentId")
+    private Long studentId;
+    @Column(name = "permissionId")
+    private Long permissionId;
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(String permissions) {
+        this.permissions = permissions;
+    }
+
+    public String getSettings() {
+        return settings;
+    }
+
+    public void setSettings(String settings) {
+        this.settings = settings;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public Long getPermissionId() {
+        return permissionId;
+    }
+
+    public void setPermissionId(Long permissionId) {
+        this.permissionId = permissionId;
+    }
+}
